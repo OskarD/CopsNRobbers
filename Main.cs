40c45970faa4430097b041584d775014a8977018
Oskar Danielsson <admin@oskard.me>
2019-04-03 17:38:06 +0200

    Set up project

---
+++ Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using GTANetworkAPI;
+
+namespace Server
+{
+    public class Main : Script
+    {
+        public Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        [Command("test")]
+        public void CMD_Test(Client client)
+        {
+            client.SendChatMessage("Test");
+        }
+    }
+}
